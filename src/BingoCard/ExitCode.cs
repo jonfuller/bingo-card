c14cfde567f2d0aeabb0e0747740ddbab48747de
Jon Fuller <jcfuller@sep.com>
2020-10-12 13:49:27 -0400

    prints a console-based bingo card

---
+++ src/BingoCard/ExitCode.cs
@@ -0,0 +1,8 @@
+﻿namespace BingoCard
+{
+    public record ExitCode(int Value, string Message)
+    {
+        public static ExitCode InputFileNotFound(in string filename) => new ExitCode(2, $"Input file {filename} not found.");
+        public static ExitCode InputFileTooShort(in string filename, in int numLines) => new ExitCode(3, $"Input file ({filename}) does not contain enough entries. Only found {numLines}.");
+    }
+}
---
+++ src/BingoCard/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CliFx;
+
+namespace BingoCard
+{
+    class Program
+    {
+        public static async Task<int> Main() =>
+            await new CliApplicationBuilder()
+                .AddCommandsFromThisAssembly()
+                .Build()
+                .RunAsync();
+    }
+}
