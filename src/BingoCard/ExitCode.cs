23f6756838b6bebb18e6695743bc4a77db24ea4f
Jon Fuller <jonfuller@users.noreply.github.com>
2020-10-13 09:10:01 -0400

    Revert "add 'docx-card' command, fixes \#2"
    This reverts commit 97431032b5d4f6270c28960b2dbab8b8dfd108d0.

--- src/BingoCard/ExitCode.cs
+++ src/BingoCard/ExitCode.cs
@@ -1,18 +1,8 @@
-﻿using System;
-
-namespace BingoCard
+﻿namespace BingoCard
 {
     public record ExitCode(int Value, string Message)
     {
-        public static ExitCode InputFileNotFound(in string filename) =>
-            new ExitCode(2, $"Input file {filename} not found.");
-        public static ExitCode InputFileTooShort(in string filename, in int numLines) =>
-            new ExitCode(3, $"Input file ({filename}) does not contain enough entries. Only found {numLines}.");
-        public static ExitCode TemplateNotFound(string docxTemplate) =>
-            new ExitCode(4, $"Docx Template file {docxTemplate} not found.");
-        public static ExitCode MailMergeFailed(string docxTemplate, string inputFilename, AggregateException exception) =>
-            new ExitCode(5, $"MailMerge failed for template file {docxTemplate} and input file {inputFilename}. Exception details:{Environment.NewLine}{exception}");
-        public static ExitCode OutputFileExists(string outputFile) =>
-            new ExitCode(6, $"Output file {outputFile} already exists. Output file will not be overwritten.");
+        public static ExitCode InputFileNotFound(in string filename) => new ExitCode(2, $"Input file {filename} not found.");
+        public static ExitCode InputFileTooShort(in string filename, in int numLines) => new ExitCode(3, $"Input file ({filename}) does not contain enough entries. Only found {numLines}.");
     }
 }
