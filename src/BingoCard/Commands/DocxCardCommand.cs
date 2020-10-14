0d8dff534e5b27552b3d0fd6724376c4190fa4b1
Jon Fuller <jcfuller@sep.com>
2020-10-13 21:02:01 -0400

    adds pdf output support for docx cards

--- src/BingoCard/Commands/DocxCardCommand.cs
+++ src/BingoCard/Commands/DocxCardCommand.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CliFx;
 using CliFx.Attributes;
+using LibreOfficeLibrary;
 using MailMerge;
 
 namespace BingoCard.Commands
@@ -19,6 +21,12 @@ namespace BingoCard.Commands
         [CommandOption("output", 'o', Description = "Output filename.", IsRequired = false)]
         public string OutputFile { get; init; } = "output.docx";
 
+        [CommandOption("output-format", 'f', Description = "Output format.", IsRequired = false)]
+        public OutputFormat OutputFormat { get; init; } = OutputFormat.docx;
+
+        [CommandOption("overwrite-output", 'w', Description = "Overwrite output file if it already exists.", IsRequired = false)]
+        public bool OverwriteOutput { get; init; } = false;
+
         public ValueTask ExecuteAsync(IConsole console)
         {
             if (!File.Exists(InputFilename))
@@ -27,7 +35,7 @@ namespace BingoCard.Commands
             if (!File.Exists(DocxTemplate))
                 throw ExitCode.TemplateNotFound(DocxTemplate).ToCommandException();
 
-            if (File.Exists(OutputFile))
+            if (File.Exists(OutputFile) && !OverwriteOutput)
                 throw ExitCode.OutputFileExists(OutputFile).ToCommandException();
 
             var inputs = File
@@ -40,16 +48,73 @@ namespace BingoCard.Commands
                 throw ExitCode.InputFileTooShort(InputFilename, inputs.Count).ToCommandException();
 
             var card = new CardData(inputs);
-            var cells = card.Cells.ToDictionary(c => c.Id, c => c.Value);
+            var cells = card.Cells.ToDictionary(c => c.Id.ToLower(), c => c.Value);
 
-            var (mergeSuccess, mergeException) = new MailMerger().Merge(DocxTemplate, cells, OutputFile);
+            var tmpFile = Path.ChangeExtension(Path.GetTempFileName(), "docx");
+
+            var (mergeSuccess, mergeException) = new MailMerger().Merge(DocxTemplate, cells, tmpFile);
 
             if (!mergeSuccess)
             {
                 throw ExitCode.MailMergeFailed(DocxTemplate, InputFilename, mergeException).ToCommandException();
             }
 
+            if (OutputFormat == OutputFormat.pdf)
+            {
+                EnsureLibreOffice();
+
+                var tmpPdf = Path.ChangeExtension(Path.GetTempFileName(), "pdf");
+                ConvertToPdf(tmpFile, tmpPdf);
+
+                File.Move(tmpPdf, OutputFile, OverwriteOutput);
+                File.Delete(tmpPdf);
+            }
+            else
+            {
+                File.Move(tmpFile, OutputFile);
+            }
+
             return default;
         }
+
+        private void ConvertToPdf(string tmpDocx, string tmpPdf)
+        {
+            try
+            {
+                new DocumentConverter().ConvertToPdf(tmpDocx, tmpPdf);
+            }
+            catch (Exception e)
+            {
+                throw ExitCode.PdfConversionFailed(tmpDocx, e).ToCommandException();
+            }
+        }
+
+        private void EnsureLibreOffice()
+        {
+            if (!LibreOfficeUtils.IsLibreOfficeOnPath())
+            {
+                var (found, sofficePath) = LibreOfficeUtils.FindLibreOfficeBinary();
+
+                if (found)
+                {
+                    var oldValue = Environment.GetEnvironmentVariable("PATH");
+                    var newValue = oldValue + Path.PathSeparator + Path.GetDirectoryName(sofficePath);
+
+                    Environment.SetEnvironmentVariable("PATH", newValue);
+                }
+                else
+                {
+                    throw ExitCode.LibreOfficeNotFound().ToCommandException();
+                }
+            }
+            if (!LibreOfficeUtils.HasLibreOfficeProfile())
+                throw ExitCode.NoLibreOfficeProfile(LibreOfficeUtils.ExpectedProfileDir).ToCommandException();
+        }
+    }
+
+    public enum OutputFormat
+    {
+        docx,
+        pdf
     }
 }
